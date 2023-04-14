cb24d3b30c16015e93f281675241f886a745104d
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-04-14 19:16:58 +0900

    Class Refactoring
    클래스 리팩토링

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,35 +5,19 @@ using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]private float moveSpeed = 7f; //Unity 내에서 속도 변환이 가능하게 해줌 > public 
+    [SerializeField] private float moveSpeed = 7f; //Unity 내에서 속도 변환이 가능하게 해줌 > public 
+    [SerializeField] private GameInput gameInput;
     private bool isWalking;
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1; 
-        }
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        Vector3 Dir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += Dir* moveSpeed * Time.deltaTime;
-
-        isWalking = Dir != Vector3.zero;
+        isWalking = moveDir != Vector3.zero;
 
         float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, Dir, Time.deltaTime * rotateSpeed);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
     }
     public bool IsWalking()
