0b7ce5aeb617d114b1d829865126ab276d9b8094
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-04-17 22:32:28 +0900

    Event
    키보드 E 이벤트 발생

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -13,6 +13,31 @@ public class Player : MonoBehaviour
     private bool isWalking;
     private Vector3 lastInteractDir;
 
+    private void Start()
+    {
+        gameInput.OnInterAction += GameInput_OnInterAction;
+    }
+
+    private void GameInput_OnInterAction(object sender, System.EventArgs e)
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                
+            }
+        }
+    }
+
     private void Update()
     {
         HandleMovement();
