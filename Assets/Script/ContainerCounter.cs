0649128acc114000021add65495d508acd8526a7
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-04-29 21:28:56 +0900

    Tomato, Cheese Container

--- Assets/Script/ContainerCounter.cs
+++ Assets/Script/ContainerCounter.cs
@@ -2,17 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour,IKitchenObjectParent
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+
+
+    private KitchenObject kitchenObject;
+    public void Interact(Player player)
     {
-        
+        if (kitchenObject == null)
+        {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        }
+        else
+        {
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+
+    }
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+    public void SetkitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public KitchenObject GetKitchenObject()
     {
-        
+        return kitchenObject;
+    }
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
