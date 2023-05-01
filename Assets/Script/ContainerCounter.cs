df94d7f7983e1fb079cddf7211ec04c39f526a1b
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-05-01 23:48:55 +0900

    Pick up N Drop Object

--- Assets/Script/ContainerCounter.cs
+++ Assets/Script/ContainerCounter.cs
@@ -1,17 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class NewBehaviourScript : BaseCounter
+public class ContainerCounter : BaseCounter
 {
+    public event EventHandler OnPlayerGrabbedObject;
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player)
     {
-        if (!HasKitchenObject())
-        {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        }
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
     }
+    
 }
