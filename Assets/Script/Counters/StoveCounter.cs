4083a9742fe64d0ab3e9f901d6f81da96573cbab
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-05-11 23:34:15 +0900

    Stove Counter

---
+++ Assets/Script/Counters/StoveCounter.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoveCounter : BaseCounter
+{
+    // Start is called before the first frame update
+    [SerializeField] private FryingRecipeSO[] fryinRecipeSOArray;
+
+}
