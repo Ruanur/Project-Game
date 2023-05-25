e797233183c80e3f3f00bde33498e6946e8cf1e1
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-05-25 14:42:01 +0900

    Relocation Counter, Delivery Counter

---
+++ Assets/Script/Counters/DeliveryCounter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if(player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                //Only accepts Plates
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
