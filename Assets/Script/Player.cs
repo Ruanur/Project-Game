6e206b25157433c5bd3630eead32a66cb9baff3a
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-10-16 11:20:12 +0900

    서버 동기화

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.VisualScripting;
 
 public class Player : NetworkBehaviour, IKitchenObjectParent
 {
@@ -61,6 +62,10 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if(!IsOwner)
+        {
+            return;
+        } 
         HandleMovement();
         HandleInteractions();
     }
