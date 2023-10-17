ec883e4b954df425898c093467d61b2f7f96fe8b
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-10-17 14:22:19 +0900

    오브젝트 드랍

--- Assets/Script/Counters/BaseCounter.cs
+++ Assets/Script/Counters/BaseCounter.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class BaseCounter : MonoBehaviour, IKitchenObjectParent
+public class BaseCounter : NetworkBehaviour, IKitchenObjectParent
 {
 
     public static event EventHandler OnAnyObjectPlacedHere;
@@ -65,7 +65,7 @@ public class BaseCounter : MonoBehaviour, IKitchenObjectParent
 
     public NetworkObject GetNetworkObject()
     {
-        return null;
+        return NetworkObject;
     }
 
 }
