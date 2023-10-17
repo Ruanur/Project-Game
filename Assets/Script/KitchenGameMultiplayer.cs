eb5b338ac72f99dad1c9a874c41e1ee9c4498e9e
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-10-17 14:18:03 +0900

    오브젝트 픽업 동기화
    호스트 - 클라이언트 O

--- Assets/Script/KitchenGameMultiplayer.cs
+++ Assets/Script/KitchenGameMultiplayer.cs
@@ -11,7 +11,7 @@ public class KitchenGameMultiplayer : NetworkBehaviour
 
     private void Awake()
     {
-        Instance = this;
+        Instance = this; //싱글톤 패턴으로 현재 인스턴스 설정.
     }
     //싱글톤
 
@@ -25,25 +25,30 @@ public class KitchenGameMultiplayer : NetworkBehaviour
     {
         KitchenObjectSO kitchenObjectSO = GetKitchenObjectSOFromIndex(kitchenObjectSOIndex);
 
+        //주방 오브젝트 생성
         Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
 
+        //NetworkObject를 얻어와 스폰(생성)
         NetworkObject kitchenObjectNetworkObject = kitchenObjectTransform.GetComponent<NetworkObject>();
         kitchenObjectNetworkObject.Spawn(true);
 
+        //KitchenObject 컴포넌트를 가져와 부모 설정
         KitchenObject kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
-
         kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
         IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
         kitchenObject.SetKitchenObjectParent(kitchenObjectParent);
-    }//서버간 오브젝트 생성 동기화
+    }
+    //서버간 오브젝트 생성 동기화
 
     private int GetKitchenObjectSOIndex(KitchenObjectSO kitchenObjectSO)
     {
+        //KitchenObjectSO를 목록에서 찾아 인덱스 반환
         return kitchenObjectListSO.kitchenObjectSOList.IndexOf(kitchenObjectSO);
     }
 
     private KitchenObjectSO GetKitchenObjectSOFromIndex(int kitchenObjectSOIndex)
     {
+        //주어진 인덱스를 사용해 KitcheneObjectSO를 반환
         return kitchenObjectListSO.kitchenObjectSOList[kitchenObjectSOIndex];
     }
 }
--- Assets/Script/KitchenObject.cs
+++ Assets/Script/KitchenObject.cs
@@ -23,13 +23,28 @@ public class KitchenObject : NetworkBehaviour
 
     public void SetKitchenObjectParent(IKitchenObjectParent kitchenObjectParent)
     {
+        SetKitchenObjectParentServerRpc(kitchenObjectParent.GetNetworkObject());
+    }
+
+    [ServerRpc(RequireOwnership = false)] //소유권 요구 = false, 이는 소유자(Host) 외에 다른 클라이언트에서도 정보를 공유할 수 있음을 의미함.
+    private void SetKitchenObjectParentServerRpc(NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        SetKitchenObjectParentClientRpc(kitchenObjectParentNetworkObjectReference);
+    }
+
+    [ClientRpc]
+    private void SetKitchenObjectParentClientRpc(NetworkObjectReference kitchenObjectParentNetworkObjectReference)
+    {
+        kitchenObjectParentNetworkObjectReference.TryGet(out NetworkObject kitchenObjectParentNetworkObject);
+        IKitchenObjectParent kitchenObjectParent = kitchenObjectParentNetworkObject.GetComponent<IKitchenObjectParent>();
+
         if (this.kitchenObjectParent != null)
         {
             this.kitchenObjectParent.ClearKitchenObject();
         }
 
         this.kitchenObjectParent = kitchenObjectParent;
-        if(kitchenObjectParent.HasKitchenObject())
+        if (kitchenObjectParent.HasKitchenObject())
         {
             Debug.LogError("Counter already has a KitchenObject!");
         }
@@ -39,7 +54,6 @@ public class KitchenObject : NetworkBehaviour
         followTransform.SetTargetTransform(kitchenObjectParent.GetKitchenObjectFollowTransform());
     }
 
-    
     public IKitchenObjectParent GetKitchenObjectParent()
     {
         return kitchenObjectParent;
