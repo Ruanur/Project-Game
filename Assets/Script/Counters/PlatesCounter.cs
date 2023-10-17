9e81205f8f6972706a7d972e7da214f7f5bd9f20
Minkj <91407973+Ruanur@users.noreply.github.com>
2023-10-17 19:27:07 +0900

    접시 카운터 동기화

--- Assets/Script/Counters/PlatesCounter.cs
+++ Assets/Script/Counters/PlatesCounter.cs
@@ -1,48 +1,86 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class PlatesCounter : BaseCounter
 {
+
+
     public event EventHandler OnPlateSpawned;
     public event EventHandler OnPlateRemoved;
 
+
     [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+
 
     private float spawnPlateTimer;
     private float spawnPlateTimerMax = 4f;
     private int platesSpawnedAmount;
-    private int platesSpawnedAmountMAX = 4;
+    private int platesSpawnedAmountMax = 4;
+
 
     private void Update()
     {
+        if (!IsServer)
+        {
+            return;
+        }
+
         spawnPlateTimer += Time.deltaTime;
         if (spawnPlateTimer > spawnPlateTimerMax)
         {
             spawnPlateTimer = 0f;
-             
-            if (KitchenGameManager.Instance.IsGamePlaying() && platesSpawnedAmount < platesSpawnedAmountMAX)
-            {
-                platesSpawnedAmount++;
 
-                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            if (KitchenGameManager.Instance.IsGamePlaying() && platesSpawnedAmount < platesSpawnedAmountMax)
+            {
+                SpawnPlateServerRpc();
             }
         }
     }
-    
+
+    [ServerRpc]
+    private void SpawnPlateServerRpc()
+    {
+        SpawnPlateClientRpc();
+    }
+
+    [ClientRpc]
+    private void SpawnPlateClientRpc()
+    {
+        platesSpawnedAmount++;
+
+        OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+    }
+
     public override void Interact(Player player)
     {
-        if(!player.HasKitchenObject())
+        if (!player.HasKitchenObject())
         {
-            if (platesSpawnedAmount >0)
+            // Player is empty handed
+            if (platesSpawnedAmount > 0)
             {
-                platesSpawnedAmount--;
-
+                // There's at least one plate here
                 KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
 
-                OnPlateRemoved?.Invoke(this, EventArgs.Empty);  
-            }    
+                InteractLogicServerRpc();
+            }
         }
     }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractLogicServerRpc()
+    {
+        InteractLogicClientRpc();
+    }
+
+    [ClientRpc]
+    private void InteractLogicClientRpc()
+    {
+        platesSpawnedAmount--;
+
+        OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+    }
+
 }
